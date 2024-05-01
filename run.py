7b86b3927a3f4a54e2563bf3a24ced573140bc68
Osman Bulut <99302852+osmanbulutedu@users.noreply.github.com>
2024-05-01 17:47:03 -0400

    Update run.py
    I made some little changes

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 import logging
-
+import os
 from app import create_app
 
 
 app = create_app()
+@app.route('/')
+def homepage():
+    return "it works"
+
 
 if __name__ == "__main__":
     logging.info("Flask app started")
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
